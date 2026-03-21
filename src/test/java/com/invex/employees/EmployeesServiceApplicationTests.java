6036411184633fa15889930bcd84b8e12c4598dc
Jorge Ventura <jacmen2005@gmail.com>
2026-03-21 08:09:12 -0600

    First commit employees service

---
+++ src/test/java/com/invex/employees/EmployeesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invex.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
